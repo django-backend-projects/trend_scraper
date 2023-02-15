074304597fb8d3ac2995bfaf0ed4cb507aabc228
galandarovisa <isa.g@labrin.tech>
2023-02-16 01:38:57 +0400

    .

--- app/app/settings.py
+++ app/app/settings.py
@@ -90,10 +90,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_IMPORTS = (
-    'core.tasks',
-)
+# CELERY_RESULT_BACKEND = 'django-db'
+# CELERY_IMPORTS = (
+#     'core.tasks',
+# )
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Database
 
