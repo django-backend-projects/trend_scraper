ae2798f4f087a2782a22e46cfefcdd2af1286b25
galandarovisa <isa.g@labrin.tech>
2023-02-26 16:24:35 +0400

    client admin updated

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Account
 
-admin.site.register(Account)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        "client_id",
+        "email",
+        "password",
+        "processed_at"
+    )
