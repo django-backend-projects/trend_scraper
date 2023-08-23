71a3c571e3f06c94c36193ea53443585da6ccfdf
taleh_ilgar <taleh.ilqar8@gmail.com>
2023-08-24 03:33:36 +0400

    -

--- isa.py
+++ isa.py
@@ -4,7 +4,11 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 import time
+
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 chrome_driver_path = "/home/safex/chromedriver"
@@ -12,7 +16,7 @@ chrome_driver_path = "/home/safex/chromedriver"
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"
 
 
-def login_to_asan(UserID, FIN, PassWord):
+def login_to_asan(DecID, FIN, PassWord):
     print("======================  başladıq =========================")
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
@@ -24,6 +28,7 @@ def login_to_asan(UserID, FIN, PassWord):
     driver.get(
         "https://asanlogin.my.gov.az/auth?origin=https:%2F%2Fe.customs.gov.az%2Fauth%2Fasan"
     )
+    # driver.maximize_window()
     time.sleep(4)
     print("======================  asan loginleshdi =========================")
     # must be click tag which it inneHTML is equal İdentifikasiya nömrəsi ilə
@@ -60,24 +65,120 @@ def login_to_asan(UserID, FIN, PassWord):
     # //*[@id="root"]/div[1]/div[1]/div[2]/div/div/section/div/div[2]/div/div[2]/div
 
     next_button = driver.find_element(
-        By.XPATH, '//*[@id="root"]/div[1]/div[1]/div[2]/div/div/section/div/div[2]/div/div[2]/div'
+        By.XPATH,
+        '//*[@id="root"]/div[1]/div[1]/div[2]/div/div/section/div/div[2]/div/div[2]/div',
     )
     next_button.click()
-
-
-
-
-
-    # get local url + for-individuals
-    # driver.get("https://e.customs.gov.az/for-individuals/post-declaration")
     time.sleep(4)
 
-    # js path document.querySelector("body > div.jss69.test-modal > div.MuiPaper-root.jss70.jss177.MuiPaper-elevation1.MuiPaper-rounded > div.jss71.jss176 > div:nth-child(1) > div.MuiCardHeader-root.jss224 > div > button > span > span")
-    # element_js_path = driver.execute_script("return document.querySelector('body > div.jss69.test-modal > div.MuiPaper-root.jss70.jss177.MuiPaper-elevation1.MuiPaper-rounded > div.jss71.jss176 > div:nth-child(1) > div.MuiCardHeader-root.jss224 > div > button > span > span');")
+    items = driver.find_elements(By.CLASS_NAME, "jss275")
+    for index, item in enumerate(items):
+        track_span = item.find_element(By.CLASS_NAME, "MuiTypography-body1").text
+        track_number = track_span.split(":")[1].strip()
+        if DecID == track_number:
+            if index != 0:
+                svg_icon = item.find_element(By.TAG_NAME, "svg")
+                svg_icon.click()
+                time.sleep(2)
+            body_ul = item.find_element(By.CLASS_NAME, "MuiList-dense")
+            body_ul_items = body_ul.find_elements(By.TAG_NAME, "li")
+            print("invoys", body_ul_items[2].text)
+            invoys_price = body_ul_items[2].text
 
-    time.sleep(400)
+            time.sleep(4)
+            confirm_button = item.find_element(
+                By.CLASS_NAME, "MuiButton-containedPrimary"
+            )
+            confirm_button.click()
+            time.sleep(2)
+
+            next_button2 = driver.find_element(
+                By.CLASS_NAME, "MuiButton-containedPrimary"
+            )
+            next_button2.click()
+            time.sleep(2)
+
+            next_button3 = driver.find_element(
+                By.CLASS_NAME, "MuiButton-containedPrimary"
+            )
+            next_button3.click()
+
+            time.sleep(2)
+            next_button4 = driver.find_element(By.CLASS_NAME, "MuiButton-contained")
+            next_button4.click()
+
+            time.sleep(2)
+            form_tag = driver.find_element(By.TAG_NAME, "form")
+            form_inputs = form_tag.find_elements(
+                By.CLASS_NAME, "MuiOutlinedInput-input"
+            )
+            popups = form_tag.find_elements(
+                By.CLASS_NAME, "MuiOutlinedInput-inputAdornedEnd"
+            )
+            first_popup = popups[0]
+            first_popup.send_keys("Paltar")
+            paltar_option = WebDriverWait(driver, 3).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//*[contains(text(), 'Paltar')]")
+                )
+            )
+            paltar_option.click()
+            time.sleep(2)
+
+            second_popup = popups[1]
+            second_popup.send_keys("Digər")
+            diger_option = WebDriverWait(driver, 3).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//*[contains(text(), 'Digər')]")
+                )
+            )
+            diger_option.click()
+            time.sleep(2)
 
 
-  
+            third_popup = popups[2]
+            third_popup.send_keys("Türk lirəsi")
+            turk_lirasi_option = WebDriverWait(driver, 3).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, "//*[contains(text(), 'Türk lirəsi')]")
+                )
+            )
+            turk_lirasi_option.click()
+            
 
-login_to_asan("123456", "1WMBK3Q", "Farhad1903")
+            invoys_price_input = form_inputs[2]
+            invoys_price_input.send_keys(invoys_price)
+
+            invoys_price_input = form_inputs[2]
+            invoys_price_input.send_keys(invoys_price)
+            time.sleep(3)
+
+            quantity_select_input = form_inputs[5]
+            quantity_select_input.send_keys("1")
+            time.sleep(3)
+
+            next_button5 = form_tag.find_element(
+                By.CLASS_NAME, "MuiButton-containedPrimary"
+            )
+            next_button5.click()
+
+            time.sleep(2)
+            # class jss445
+            checkbox_input = driver.find_element(By.CLASS_NAME, "jss445")
+            checkbox_input.click()
+
+            time.sleep(2)
+            confirm_button = driver.find_element(By.CSS_SELECTOR, 'button[test-id="approve"]')
+            confirm_button.click()
+
+            time.sleep(3)
+            driver.quit()
+
+        else:
+            print("not found")
+
+            # must be select Türk lirəsi option
+
+
+login_to_asan("KLI1357262", "1WMBK3Q", "Farhad1903")
+# login_to_asan("123456", "105YYPL", "Aa123456")
